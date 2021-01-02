22c9dced4fbe6ac2d795b5411b291f90c3c32ed5
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2021-01-02 16:34:35 -0300

    aplicacao-constraint

--- Alura Viagens/Home/Controller/ViewController.swift
+++ Alura Viagens/Home/Controller/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.labelTitulo.text = viagemAtual.titulo
         cell.labelQuantidadedeDias.text = " \(viagemAtual.quantidadeDeDias)  dias"
-        cell.labelPreco.text = viagemAtual.preco
+        cell.labelPreco.text = "R$ \(viagemAtual.preco)"
         cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem) // inserir imagem
         
         cell.imagemViagem.layer.cornerRadius = 10 // arredondar cantos
@@ -51,7 +51,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 175
+        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 175 : 260
+        
     }
 
 
