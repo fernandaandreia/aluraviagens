d32758428166b7d1eb5d3c237b0b4caaf69c2c12
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2021-01-02 13:11:34 -0300

    listar-titulos-viagens

--- Alura Viagens/ViewController.swift
+++ Alura Viagens/ViewController.swift
@@ -7,12 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    
+    // MARK : IBOutlets
+    @IBOutlet weak var tabelaViagens: UITableView!
+    @IBOutlet weak var viewHoteis: UIView!
+    @IBOutlet weak var viewPacotes: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tabelaViagens.dataSource = self
+        self.viewPacotes.layer.cornerRadius = 10
+        self.viewHoteis.layer.cornerRadius = 10
+        
     }
+    
+    
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let viagemAtual = listaViagens[indexPath.row]
+        
+        cell.textLabel?.text = viagemAtual.titulo
+        return cell
+    }
+    
+    
 
 
 }
