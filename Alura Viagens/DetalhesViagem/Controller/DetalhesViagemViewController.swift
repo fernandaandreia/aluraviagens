bb5a1d50561be9fcd172227808f847464d5bace8
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2021-01-04 11:23:25 -0300

    customizar-tipos-teclados

--- Alura Viagens/DetalhesViagem/Controller/DetalhesViagemViewController.swift
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagemViewController.swift
@@ -10,31 +10,36 @@ import UIKit
 class DetalhesViagemViewController: UIViewController {
 
     @IBOutlet weak var imagemPacoteViagem: UIImageView!
-    
     @IBOutlet weak var labelTituloPacoteViagem: UILabel!
     @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
     @IBOutlet weak var labelDataViagem: UILabel!
     @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
-    
     @IBAction func botaoVoltar(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+    @IBOutlet weak var scrollPrincipal: UIScrollView!
+    
     
     
     var pacoteSelecionado:PacoteViagem? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        NotificationCenter.default.addObserver(self, selector: #selector (aumentarScroll(Notification:)), name: .UIKeyboardWillShow, object: nil)
 
         if let pacote = pacoteSelecionado {
             self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
             self.labelTituloPacoteViagem.text = pacote.viagem.titulo
             self.labelDescricaoPacoteViagem.text = pacote.descricao
             self.labelDataViagem.text = pacote.dataViagem
-            self.labelPrecoPacoteViagem = pacote.viagem.preco
+//            self.labelPrecoPacoteViagem = pacote.viagem.preco
 
         }
-    
+        func aumentarScroll(notification: Notification){
+//            print("Teste teclado")
+            self.scrollPrincipal.contentSize = CGSize(width: self.scrollPrincipal.frame.width, height: self.scrollPrincipal.frame.height + 320)
+        }
     
     }
     
